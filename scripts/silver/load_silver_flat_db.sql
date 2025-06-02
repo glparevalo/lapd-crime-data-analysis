e8d8217688bf3a11b256e8be005570a6aa844d20
Gabriel Arevalo <lyanearevalo@gmail.com>
2025-06-02 18:29:48 +0800

    update loading silver layer

--- scripts/silver/load_silver_flat_db.sql
+++ scripts/silver/load_silver_flat_db.sql
@@ -24,6 +24,8 @@ BEGIN
         -- Set start time to measure the processing time
         SET @start_time = GETDATE();
 
+        
+
         TRUNCATE TABLE silver.lapd_crime_database;
         PRINT('Truncating silver.lapd_crime_database...');
 
@@ -33,7 +35,7 @@ BEGIN
             date_reported,
             date_occurred,
             time_occurred,
-            area,
+            area_id,
             area_name,
             report_district_no,
             part,
@@ -49,7 +51,6 @@ BEGIN
             status_cd,
             status_desc,
             crime_location,
-            cross_street,
             crime_lat,
             crime_lon
         )
@@ -64,43 +65,46 @@ BEGIN
             report_district_no,
             part,
             crime_cd,
-            REPLACE(REPLACE(UPPER(TRIM(crime_cd_desc)), ';', ': '), '  ',' ') AS new,
+            REPLACE(REPLACE(UPPER(TRIM(crime_cd_desc)), ';', ': '), '  ',' ') AS crime_cd_desc,
             CASE 
                 WHEN vict_age < 0 THEN ABS(vict_age)
-                WHEN vict_age = 0 THEN null
                 ELSE vict_age 
-            END AS vict_age,
+            END as vict_age,
             CASE
                 WHEN UPPER(TRIM(vict_sex)) = 'H' THEN 'Other'
-                WHEN UPPER(TRIM(vict_sex)) = '-' or UPPER(trim(vict_sex)) = 'X' or upper(trim(vict_sex)) IS NULL THEN 'N/A'
+                WHEN UPPER(TRIM(vict_sex)) = '-' or UPPER(TRIM(vict_sex)) = 'X' or UPPER(TRIM(vict_sex)) IS NULL THEN 'N/A'
                 WHEN UPPER(TRIM(vict_sex)) = 'M' THEN 'Male'
                 WHEN UPPER(TRIM(vict_sex)) = 'F' THEN 'Female'
                 ELSE UPPER(TRIM(vict_sex))
             END AS vict_sex,
             CASE	
-                WHEN UPPER(trim(vict_descent)) is null or UPPER(trim(vict_descent)) = '-' THEN 'X'
-                ELSE UPPER(trim(vict_descent))
+                WHEN UPPER(TRIM(vict_descent)) is null or UPPER(TRIM(vict_descent)) = '-' THEN 'X'
+                ELSE UPPER(TRIM(vict_descent))
             END AS vict_descent,
-            premis_cd,
-            UPPER(trim(premis_desc)) AS premis_desc,
-            weapon_used_cd,
-            UPPER(trim(weapon_desc)) AS weapon_desc,
-            UPPER(trim(status_cd)) AS status_cd,
+            ISNULL(premis_cd, 0) as premis_cd,
+            case
+				when premis_cd is null and premis_desc is null then 'N/A'
+				when premis_cd is not null and premis_desc is null then 'NO DESCRIPTION GIVEN'
+				ELSE premis_desc
+			END AS premis_desc,
+            ISNULL(weapon_used_cd, 0),
+            ISNULL(UPPER(TRIM(weapon_desc)), 'N/A') AS weapon_desc,
+            ISNULL(UPPER(TRIM(status_cd)), 'N/A') AS status_cd,
             CASE
                 WHEN UPPER(TRIM(status_cd)) = 'CC' and status_desc = 'UNK' THEN 'Case Closed'
-                WHEN UPPER(TRIM(status_cd)) is null and status_desc = 'UNK' THEN 'Unknown'
+                WHEN UPPER(TRIM(status_cd)) is null and status_desc = 'UNK' OR UPPER(TRIM(status_cd)) = 'N/A' THEN 'Unknown'
                 WHEN UPPER(TRIM(status_cd)) = 'JA' THEN 'Juvenile Arrest'
                 WHEN UPPER(TRIM(status_cd)) = 'JO' THEN 'Juvenile Other'
                 WHEN UPPER(TRIM(status_cd)) = 'AO' THEN 'Adult Other'
                 WHEN UPPER(TRIM(status_cd)) = 'AA' THEN 'Adult Arrest'
                 WHEN UPPER(TRIM(status_cd)) = 'IC' THEN 'Investigation Continued'
             END AS status_desc,
-            REPLACE(REPLACE(REPLACE(
+            TRIM(REPLACE(REPLACE(REPLACE(
                 REPLACE(REPLACE(REPLACE(
                     REPLACE(REPLACE(REPLACE(TRIM(crime_location), '  ',' ')
                     , '  ', ' '), '  ', ' '), '  ', ' '),
                 '  ', ' '), '  ', ' '), '  ', ' '),
-            '  ', ' '), '  ', ' ') AS crime_location,
+            '  ', ' '), '  ', ' ') + ' ' +
             CASE
 				WHEN UPPER(TRIM(cross_street)) IS NULL THEN ''
 				ELSE REPLACE(REPLACE(REPLACE(
@@ -109,10 +113,10 @@ BEGIN
 							, '  ', ' '), '  ', ' '), '  ', ' '),
 						'  ', ' '), '  ', ' '), '  ', ' '),
 					'  ', ' '), '  ', ' ')
-				END AS cross_street,
-            crime_lat,
-            crime_lon
-        FROM bronze.lapd_crime_database
+				END) AS crime_location,
+			ISNULL(CAST(cast(crime_lat as decimal(8,5)) AS nvarchar(150)), '0') AS crime_lat,
+            ISNULL(CAST(cast(crime_lon as decimal(8,5)) AS nvarchar(150)), '0') AS crime_lon
+        FROM bronze.lapd_crime_database;
 
 		SET @end_time = GETDATE();
         PRINT('Done processing ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds');
@@ -128,6 +132,104 @@ BEGIN
     END CATCH
 END;
 
-GO
 
-EXEC silver.load_silver_flat_db
+create or alter PROCEDURE silver.load_silver_normalized_db as 
+    BEGIN
+        TRY
+
+
+    -- Load Location Table    
+    TRUNCATE TABLE silver.location_table;
+
+    INSERT INTO silver.location_table(
+        sk_location_key,
+        premis_cd,
+        crime_location,
+        crime_lat,
+        crime_lon
+    )
+
+    select
+        ROW_NUMBER() over (order by crime_location, premis_cd) as sk_location_key,
+        premis_cd,
+        crime_location,
+        crime_lat,
+        crime_lon 
+    from(
+    select distinct
+        premis_cd,
+        crime_location,
+        crime_lat,
+        crime_lon
+    from silver.lapd_crime_database
+    ) t
+
+    GO
+
+    -- Load Area Table
+    TRUNCATE TABLE silver.area_table;
+
+    INSERT INTO silver.area_table(
+        area,
+        area_name
+    )
+    select distinct
+        area,
+        area_name
+    from silver.lapd_crime_database
+    order by area;
+
+    GO
+
+    -- Load Premise Table
+    TRUNCATE TABLE silver.premis_table;
+
+    INSERT INTO silver.premis_table(
+        premis_cd,
+        premis_desc
+    )
+    select distinct
+        premis_cd,
+        premis_desc
+    from silver.lapd_crime_database
+	order bY premis_cd
+
+
+    TRUNCATE TABLE silver.crime_setting;
+    insert into silver.crime_setting(
+        dr_no,
+        date_reported,
+        date_occurred,
+        time_occurred,
+        area,
+        sk_location_key
+    )
+
+    select 
+        m.dr_no,
+        m.date_reported,
+        date_occurred,
+        time_occurred,
+        area,
+        l.sk_location_key
+    from silver.lapd_crime_database m
+    left join silver.location_table l
+    on m.premis_cd = l.premis_cd
+    and m.crime_location = l.crime_location
+    and m.crime_lat = l.crime_lat
+    and m.crime_lon = l.crime_lon
+
+    /*
+    ========================================================
+
+    ========================================================
+    */
+
+
+    end TRY
+    begin CATCH
+    end catch
+
+
+
+end
