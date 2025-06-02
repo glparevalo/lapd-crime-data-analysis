4d6d23ba35b602fff4cff4b2b1631f1182750316
Gabriel Arevalo <lyanearevalo@gmail.com>
2025-06-02 18:29:19 +0800

    update ddl_silver

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -16,38 +16,36 @@ Silver Flat Table:
 */
 
 -- Silver LAPD Crime Database
-
 IF OBJECT_ID('silver.lapd_crime_database', 'U') IS NOT NULL
-	DROP TABLE silver.lapd_crime_database;
+    DROP TABLE silver.lapd_crime_database;
+
 GO
 
 CREATE TABLE silver.lapd_crime_database (
-	dr_no					INT,
-	date_reported			DATE,
-	date_occurred			DATE,
-	time_occurred			TIME(0),
-	area					INT,
-	area_name				NVARCHAR(150),
-	report_district_no		INT,
-	part					INT,
-	crime_cd				INT,
-	crime_cd_desc			NVARCHAR(150),
-	
-	mo_codes				NVARCHAR(150),
-	vict_age				INT,
-	vict_sex				NVARCHAR(150),
-	vict_descent			NVARCHAR(150),
-	premis_cd				INT,
-	premis_desc				NVARCHAR(150),
-	weapon_used_cd			INT,
-	weapon_desc				NVARCHAR(150),
-	status_cd				NVARCHAR(150),
-	status_desc				NVARCHAR(150),
-	
-	crime_location			NVARCHAR(150),
-	cross_street			NVARCHAR(150),
-	crime_lat				FLOAT,
-	crime_lon				FLOAT
+    dr_no					INT,
+    date_reported			DATE,
+    date_occurred			DATE,
+    time_occurred			TIME(0),
+    area_id					INT,
+    area_name				NVARCHAR(150),
+    report_district_no		INT,
+    part					INT,
+    crime_cd				INT,
+    crime_cd_desc			NVARCHAR(150),
+            
+    vict_age				INT,
+    vict_sex				NVARCHAR(150),
+    vict_descent			NVARCHAR(150),
+    premis_cd				INT,
+    premis_desc				NVARCHAR(150),
+    weapon_used_cd			INT,
+    weapon_desc				NVARCHAR(150),
+    status_cd				NVARCHAR(150),
+    status_desc				NVARCHAR(150),
+            
+    crime_location			NVARCHAR(150),
+    crime_lat				NVARCHAR(150),
+    crime_lon				NVARCHAR(150)
 );
 
 GO
@@ -59,21 +57,25 @@ GO
 
 */
 
--- Crime Setting
+-- =================== Crime Setting =====================
 
+-- Crime Setting
 IF OBJECT_ID('silver.crime_setting', 'U') IS NOT NULL
 	DROP TABLE silver.crime_setting;
 GO
 
 CREATE TABLE silver.crime_setting (
-	dr_no					INT,
+	dr_no					INT PRIMARY KEY,
 	date_reported			DATE,
 	date_occurred			DATE,
 	time_occurred			TIME(0), 
 	area					INT,
-	premis_cd				INT
+	sk_location_key			INT
 );
 
+GO
+
+-- Area Table
 IF OBJECT_ID('silver.area_table', 'U') IS NOT NULL
 	DROP TABLE silver.area_table;
 GO
@@ -81,27 +83,38 @@ GO
 CREATE TABLE silver.area_table (
 	area					INT,
 	area_name				NVARCHAR(150),
-	report_district_no		INT,
 );
 
 GO
 
-IF OBJECT_ID('silver.premise_table', 'U') IS NOT NULL
-	DROP TABLE silver.premise_table;
+-- Location Table
+IF OBJECT_ID('silver.location_table', 'U') IS NOT NULL
+	DROP TABLE silver.location_table;
 GO
 
-CREATE TABLE silver.premise_table (
+CREATE TABLE silver.location_table (
+        sk_location_key		INT	PRIMARY KEY,
+        premis_cd			INT,
+        crime_location		NVARCHAR(150),
+        crime_lat			NVARCHAR(150),
+        crime_lon			NVARCHAR(150)
+);
+
+GO
+
+-- Premise Table
+IF OBJECT_ID('silver.premis_table', 'U') IS NOT NULL
+	DROP TABLE silver.premis_table;
+GO
+
+CREATE TABLE silver.premis_table (
 	premis_cd				INT,
 	premis_desc				NVARCHAR(150),
-	crime_location			NVARCHAR(150),
-	cross_street			NVARCHAR(150),
-	crime_lat				FLOAT,
-	crime_lon				FLOAT
 );
 
 GO
 
--- Crime Method
+-- =================== Crime Method =====================
 
 IF OBJECT_ID('silver.crime_method', 'U') IS NOT NULL
 	DROP TABLE silver.crime_method;
@@ -127,6 +140,8 @@ CREATE TABLE silver.crime_table (
 
 GO
 
+
+-- Create Weapon Table
 IF OBJECT_ID('silver.weapon_table', 'U') IS NOT NULL
 	DROP TABLE silver.weapon_table;
 GO
