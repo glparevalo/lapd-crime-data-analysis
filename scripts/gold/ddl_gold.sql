e40533a7e139a3c2a9baeb911d651c0ce84b8ed3
Gabriel Arevalo <lyanearevalo@gmail.com>
2025-06-09 11:04:26 +0800

    edit ddl_gold

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -27,28 +27,23 @@ IF OBJECT_ID('gold.dim_method', 'V') IS NOT NULL
     DROP VIEW gold.dim_method;
 GO
 
-CREATE TABLE gold.dim_method(
-	method_key		INT,
-	part			NVARCHAR(150),
-	category		NVARCHAR(150),
-	crime			NVARCHAR(150),
-	weapon_used		NVARCHAR(150)
-);
-GO
+create view gold.dim_method as 
+	select 
+		sk_method_key as method_key,
+		p.part_name as part,
+		p.part_category as category,
+		c.crime_cd_desc as crime_committed,
+		w.weapon_desc as weapon_used
+	from silver.dim_method m
+	left join silver.sub_dim_part p
+	on m.part = p.part
 
--- ============================================
---		DIMENSION: Status
--- ============================================
+	left join silver.sub_dim_crime c
+	on m.crime_cd = c.crime_cd
 
-IF OBJECT_ID('gold.dim_status', 'V') IS NOT NULL
-    DROP VIEW gold.dim_status;
-GO
+	left join silver.sub_dim_weapon w
+	on m.weapon_used_cd = w.weapon_used_cd;
 
-CREATE TABLE gold.dim_status(
-	status_key			INT,
-	reporting_district_number	INT,
-	status_description		NVARCHAR(150)
-)
 GO
 
 -- ============================================
@@ -59,13 +54,27 @@ IF OBJECT_ID('gold.dim_location', 'V') IS NOT NULL
     DROP VIEW gold.dim_location;
 GO
 
-CREATE TABLE gold.dim_location(
-	location_key	            	INT,
-	premise			    	NVARCHAR(150),
-	crime_address	     		NVARCHAR(150),
-	crime_address_latitude		DECIMAL(8,5),
-	crime_address_longitude		DECIMAL(8,5)
-)
+create or alter view gold.dim_location as
+	select
+		m.sk_location_key as location_key,
+		a.area_name as crime_area,
+		p.premis_desc as crime_premise,
+		ad.crime_address,
+		ad.crime_lat as crime_latitude,
+		round(cast(ad.crime_lat as decimal(8,5)), 2) as rounded_latitude,
+		ad.crime_lon as crime_longitude,
+		round(cast(ad.crime_lon as decimal(8,5)), 2) as rounded_longitude
+	from silver.dim_location m
+
+	left join silver.sub_dim_premis p
+	on m.premis_cd = p.premis_cd
+
+	left join silver.sub_dim_area a
+	on m.area = a.area
+
+	left join silver.sub_dim_address ad
+	on m.sk_address_key = ad.sk_address_key;
+
 GO
 
 -- ============================================
@@ -76,32 +85,77 @@ IF OBJECT_ID('gold.dim_victim_profile', 'V') IS NOT NULL
     DROP VIEW gold.dim_victim_profile;
 GO
 
-CREATE TABLE gold.dim_victim_profile(
-	victim_profile_key		INT,
-	victim_age			INT,
-	victim_sex			NVARCHAR(50),
-	victim_descent			NVARCHAR(150)
-)
+create or alter view gold.dim_victim_profile as
+	select 
+		sk_vict_key as victim_key,
+		vict_age as victim_age,
+		CASE 
+		WHEN vict_age IS NULL OR vict_age <= 0 THEN 'NO AGE DISCLOED'
+		WHEN vict_age < 18 THEN '<18'
+		WHEN vict_age BETWEEN 18 AND 24 THEN '18 - 24'
+		WHEN vict_age BETWEEN 25 AND 34 THEN '25 - 34'
+		WHEN vict_age BETWEEN 35 AND 44 THEN '35 - 44'
+		WHEN vict_age BETWEEN 45 AND 54 THEN '45 - 54'
+		WHEN vict_age BETWEEN 55 AND 64 THEN '55 - 64'
+		ELSE '65+'
+		END AS victim_age_category,
+		vict_sex as victim_sex,
+		UPPER(d.vict_descent_desc) as victim_descent
+	from silver.dim_victim_profile m
+	left join silver.sub_dim_descent d
+	on m.vict_descent = d.vict_descent;
+
+GO
+
+-- ============================================
+--		DIMENSION: Mo Code
+-- ============================================
+
+IF OBJECT_ID('gold.dim_mo_code', 'V') IS NOT NULL
+    DROP VIEW gold.dim_mo_code;
+	
+GO
+
+create view gold.dim_mo_code as 
+	select
+		dr_no,
+		mo_code
+	from silver.dim_mo_code;
+
 GO
 
 -- ============================================
 --		FACT: Crime Event
 -- ============================================
 
-IF OBJECT_ID('gold.fact_crime_event', 'V') IS NOT NULL
-    DROP VIEW gold.fact_crime_event;
+IF OBJECT_ID('gold.fact_crime_specifics', 'V') IS NOT NULL
+    DROP VIEW gold.fact_crime_specifics;
 GO
 
-CREATE TABLE gold.fact_crime_event(
-	crime_key		INT,
-	dr_no			INT,
-	date_reported		DATE,
-	date_occurred		DATE,
-	time_occurred		TIME(0),
-	area			NVARCHAR(150),
-	location_key		INT,
-	method_key		INT,
-	victim_profile_key	INT,
-	status_key		INT
-)
-GO
+CREATE OR ALTER VIEW gold.fact_crime_specifics AS
+	select 
+		sk_report_key as report_key,
+		dr_no,
+		date_reported,
+		t.date_occurred,
+		t.time_occurred,
+		CASE 
+			WHEN time_occurred IS NULL THEN 'NO TIME DISCLOSED'
+			WHEN CAST(time_occurred AS TIME) BETWEEN '00:00:00' AND '05:59:59' THEN 'LATE NIGHT'
+			WHEN CAST(time_occurred AS TIME) BETWEEN '06:00:00' AND '11:59:59' THEN 'MORNING'
+			WHEN CAST(time_occurred AS TIME) BETWEEN '12:00:00' AND '17:59:59' THEN 'AFTERNOON'
+			WHEN CAST(time_occurred AS TIME) BETWEEN '18:00:00' AND '21:59:59' THEN 'EVENING'
+			WHEN CAST(time_occurred AS TIME) BETWEEN '22:00:00' AND '23:59:59' THEN 'NIGHT'
+			ELSE 'INVALID TIME'
+		END AS time_category,
+		report_district_no,
+		s.status_desc as report_status,
+		sk_method_key as method_key,
+		sk_location_key as location_key,
+		sk_vict_key as victim_key
+	FROM silver.fact_specifics m
+	left join silver.dim_time t
+	on m.sk_time_key = t.sk_time_key
+
+	left join silver.dim_status s
+	on m.status_cd = s.status_cd;
