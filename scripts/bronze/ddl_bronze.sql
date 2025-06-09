76264b1a9fad162e6374f29b239e0679e0116964
Gabriel Arevalo <143163497+glparevalo@users.noreply.github.com>
2025-06-09 14:38:06 +0800

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -4,8 +4,8 @@
 =============================================================================
 
 Bronze Table:
-	This script is used to create the Bronze table which consists of the raw 
-	data of the LAPD Crime Dataset. This table is used as a basis to
+	This script is used to create the Bronze table which consists of the 
+	raw data of the LAPD Crime Dataset. This table is used as a basis to
 	create views that will help normalize the dataset. This script drops 
 	the table if it already exists then creates a new one.
 
@@ -17,28 +17,28 @@ IF OBJECT_ID('bronze.lapd_crime_data', 'U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.lapd_crime_data (
-	dr_no					INT,
+	dr_no				INT,
 	date_reported			datetime,
 	date_occurred			date,
 	time_occurred			time,
-	area					INT,
-	area_name				NVARCHAR(200),
+	area				INT,
+	area_name			NVARCHAR(200),
 	report_district_no		INT,
-	part					INT,
-	crime_cd				INT,
+	part				INT,
+	crime_cd			INT,
 	crime_cd_desc			NVARCHAR(200),
-	mo_codes				NVARCHAR(200),
-	vict_age				INT,
-	vict_sex				NVARCHAR(200),
+	mo_codes			NVARCHAR(200),
+	vict_age			INT,
+	vict_sex			NVARCHAR(200),
 	vict_descent			NVARCHAR(200),
-	premis_cd				INT,
-	premis_desc				NVARCHAR(200),
+	premis_cd			INT,
+	premis_desc			NVARCHAR(200),
 	weapon_used_cd			INT,
-	weapon_desc				NVARCHAR(200),
-	status_cd				NVARCHAR(200),
-	status_desc				NVARCHAR(200),
+	weapon_desc			NVARCHAR(200),
+	status_cd			NVARCHAR(200),
+	status_desc			NVARCHAR(200),
 	crime_location			NVARCHAR(200),
 	cross_street			NVARCHAR(200),
-	crime_lat				DECIMAL(8,5),
-	crime_lon				DECIMAL(8,5)
+	crime_lat			DECIMAL(8,5),
+	crime_lon			DECIMAL(8,5)
 );
